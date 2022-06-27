1526ed748cb1997801203fca3d4c58fd3e36497a
Andrea Rosentel <andrearosentel@gmail.com>
2022-06-27 13:16:54 -0700

    commit tree debc38663e2dccc713e3e3144284acf25b7283de

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const num1 = 31
+const num2 = 2
+const num3 = 64
+const num4 = 6
+function multiply() {  
+    let answer = num1 * num2
+    return answer
+}
+multiply(num1, num2)
 
-// Write your code here
+function random(answer) {
+    answer = Math.ceil(Math.random())
+}
+function mod() {
+    remainder = num3 % num4
+    return remainder
+}
+// function max()
